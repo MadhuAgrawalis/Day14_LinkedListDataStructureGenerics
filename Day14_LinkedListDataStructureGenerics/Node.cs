fa8e67488cee7ece7ab4deac545db101c809a092
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-02-06 21:58:37 +0530

    UC1

---
+++ Day14_LinkedListDataStructureGenerics/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkedList
+{
+    internal class Node
+    {
+        internal object next;
+        private int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ Day14_LinkedListDataStructureGenerics/UC1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class UC1_LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = (Node)temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(value: NewMethod(node));
+        }
+
+        private static string NewMethod(Node Node)
+        {
+            return $"{"Node.data"} inserted into linked list";
+        }
+    }
+}
